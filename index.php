e4c5dd9f029a7ee187eb403d6afa73eaf439cfcd
prince <43395338+mcoderprince@users.noreply.github.com>
2020-06-15 00:03:02 +0530

    made responsive

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<title> Live Scores &amp; Updates </title>
-	<link   rel="stylesheet" href="css/style.css" type="text/css"> </link>
 	<link rel="shortcut icon" type="image/png" href="./images/download.png"/>
+	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
 	<meta name="HandheldFriendly" content="true" />
     <meta name="apple-touch-fullscreen" content="YES" />
@@ -11,26 +11,58 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="footer, address, phone, icons" />
-</head>
-<body background="ckt.jpg">
-<div>
-	<header>
-		<h1>Live Scores</h1>
-	</header>
-	<nav>
-	   <ul>
-			<li><a href="hom.php">Menu</a></li>
-			<li><a href="liveScores.php" ""target="_blank">Live scores</a></li>
-			<li><a href="league.html">Schedule</a></li>
-			<li><a href="news.html">News</a></li>
-			<li><a href="aboutus.html">Our Team</a></li>
 
-	   </ul>
+
+    <style type="text/css">
+    	body{
+    		padding:0;
+    		margin: 0;
+    	}
+    	nav{
+    		position: sticky;
+    		top: 0px;
+    	}
+    	footer{
+    		
+    		width: 100%;
+    		text-align: center;
+    		bottom: 0;
+    	}
+    	.section{
+    		background: url("ckt.jpg");
+    		background-size: cover;
+    		height: 100vh;
+    	}
+    	a:hover{
+    		color: pink;
+    	}
+    	@media only screen and (max-width: 600px){
+    		nav a{
+    			font-size: 12px;
+    			font-weight: bold;
+
+    		}
+    		nav{
+    			height: auto;
+    			
+    		}
+    	}
+    </style>
+</head>
+<body >
+	<nav class="bg-blue-400 text-white  text-3xl">
+		<a class="mx-2 " href="hom.php">Menu</a>
+		<a class="mx-2 " href="liveScore.php">Live Score</a>
+		<a class="mx-2 " href="league.html">Schedule</a>
+		<a class="mx-2 " href="news.html">News</a>
+		<a class="mx-2 " href="aboutus.html">Our Team</a>
 	</nav>
-	<section>
-		
-	</section>
-	<footer>
+
+	<div class="section">
+		this is my content
+	</div>
+
+	<footer class="bg-black text-white text-2xl">
 		<p>copyright &copy; Pravesh maurya</p>
 	</footer>
 </body>
