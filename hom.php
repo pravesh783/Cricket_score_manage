884706118845c64a53b7d1b7d282f9d2c79a6b63
prince <43395338+mcoderprince@users.noreply.github.com>
2020-07-05 17:35:05 +0530

    made responsive

--- hom.php
+++ hom.php
@@ -3,125 +3,92 @@
 <head>
 	<title>IHL 2021</title>
     <link rel="shortcut icon" type="image/png" href="./images/download.png"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
     <meta name="HandheldFriendly" content="true" />
     <meta name="apple-touch-fullscreen" content="YES" />
-    <link rel="stylesheet" type="text/css" href="css/front.css">
-    <link rel="stylesheet" type="text/css" href="css/footer.css">
+    
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="footer, address, phone, icons" />
- 
+
     <link rel="stylesheet" href="style.css">
     
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
- 
+
     <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
- <style type="text/css">
-     body{
-        background-image: url('./images/ground3.jpg');
-     }
- </style>
+    <style type="text/css">
+        body{
+            padding:0;
+            margin: 0;
+        }
+        nav{
+           
+        }
+        .section{
+            height: 100vh;
+            position: relative;
+        }
+        footer{    
+            width: 100%;
+            text-align: center;
+            position: absolute;
+        }
+        nav{
+            text-align: center;
+        }
+        .card{
+            height: 200px;
+            position: relative;
+        }
+        a{
+            color: blue;
+            font-weight: bold;
+            font-size: 30px;
+        }
+        @media only screen and (max-width: 600px){
+            
+            nav{
+                font-weight: 13px;
+            }
+            .card{
+                width: 90%;
+            }
+        }
+    </style>
+
 </head>
 <body>
-<div class="header">	
-<img src="./images/log.png">
-
-<h1 id="heading" style="color: red; margin-top:-80px; text-align: center;"> ITM HOSTEL LEGUE</h1>
-<p class="site-slogan" style=" padding: 30px; font-weight: bold; font-size: 28px;color: red;" >|| You Don't play for the crowd you play for the Team ||</p>
-</div><div style="float: left;margin-left: 1100px;margin-top: -250px;">
-    <img src="./images/log.png">
-</div>
 
 
-<div class='container'>
-    <div class='row'>
-        <div class='btn'>
-            <h1>About IHL</h1>
-            <a href="gallery.html">Gallery</a> <br><br>  
-            <a href="league.html">League 2021</a> <br> <br>       
-               
+    <nav class="bg-blue-400 text-white ">
+        <h1>ITM HOSTEL LEGUE</h1>
+        <p>|| You Don't play for the crowd you play for the Team ||</p>
+    </nav>
+<!--nav end here-->
+
+
+    <div class="section bg-white py-3 px-3">
+        <div class="card rounded px-3 py-4 bg-green-300 mx-auto my-2 shadow-2xl">
+            <h1 class="text-4xl ">About IHL</h1>
+            <a href="gallery.html">Gallery</a>
+            <a href="league.html">League 2021</a>
         </div>
-
-        <div class='btn'>
-
-        	<h1>Student</h1>
-
-        			
-        	<a href="index.php"style="font-size: 40px;font-weight: bolder;">Login</a> <br> <br>		
-        	
+        <div class="card rounded px-3 py-4 bg-green-300 mx-auto my-2 shadow-2xl">
+            <h1 class="text-4xl ">Student</h1>
+            <a href="index.php">Login</a>
         </div>
-        <div class='btn'>
-        	<h1>Admin</h1>
-       
-        	<a href="./admin/login.php"style="font-size: 40px;font-weight: bolder;">Login</a> <br> <br>		
-        	
+        <div class="card rounded px-3 py-4 bg-green-300 mx-auto my-2 shadow-2xl">
+        <h1 class="text-4xl ">Admin</h1>
+        <a href="./admin/login.php">Login</a>
         </div>
     </div>
-    <div class='clear'></div>
-</div>
-<!-- The content of your page would go here. -->
- 
-       <div class="ab">
-        <footer class="footer-distributed">
- 
-        <div class="footer-left">
- 
-        <h3>Softcare<span>IHL</span></h3>
- 
-        <p class="footer-links">
-        <a href="https://pravesh783.github.io/">About me</a>
-    ·
-        
-    ·
     
-                <a href="https://pravesh783.github.io/">
-                <img src="./images/abt.jpg">
-                </a>
- 
-        <p class="footer-company-name">@Softacre &copy; 2020</p>
-        </div>
- 
-        <div class="footer-center">
- 
-        <div>
-        <i class="fa fa-map-marker"></i>
-        <p><span>ITM Boys Hostel,sector 7</span> Gida Gorakhpur 273209, India</p>
-        </div>
- 
-        <div>
-        <i class="fa fa-phone"></i>
-        <p>7860497542</p>
-        </div>
- 
-        <div>
-        <i class="fa fa-envelope"></i>
-        <p><a href="mailto:praveshm783@gmail.com">praveshm783@gmail.com</a></p>
-        </div>
- 
-        </div>
- 
-        <div class="footer-right">
- 
-        <p class="footer-company-about">
-        <span>About the company</span>
-    Softcare is a best team in ITM for working in project &amp; Learner.
-        </p>
- 
-        <div class="footer-icons">
- 
-        <a href="https://www.facebook.com/pravesh.maurya.18847"><i class="fa fa-facebook"></i></a>
-        <a href="https://twitter.com/Praveshm783"><i class="fa fa-twitter"></i></a>
-        <a href="https://www.linkedin.com/in/pravesh-maurya-a24516198/"><i class="fa fa-linkedin"></i></a>
-        <a href="https://github.com/pravesh783"><i class="fa fa-github"></i></a>
-        <a href="https://www.instagram.com/pravesh_maurya783/"><i class="fa fa-instagram"></i></a>
- 
-        </div>
- 
-        </div>
- 
-        </footer>
-        </div>
+
+<footer class="bg-black text-white text-2xl">
+    <p>copyright &copy; Pravesh maurya</p>
+</footer>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,60 +6,69 @@
 	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
 	<meta name="HandheldFriendly" content="true" />
-    <meta name="apple-touch-fullscreen" content="YES" />
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="footer, address, phone, icons" />
+	<meta name="apple-touch-fullscreen" content="YES" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
 
 
-    <style type="text/css">
-    	body{
-    		padding:0;
-    		margin: 0;
-    	}
-    	nav{
-    		position: sticky;
-    		top: 0px;
-    	}
-    	footer{
-    		
-    		width: 100%;
-    		text-align: center;
-    		bottom: 0;
-    	}
-    	.section{
-    		background: url("ckt.jpg");
-    		background-size: cover;
-    		height: 100vh;
-    	}
-    	a:hover{
-    		color: pink;
-    	}
-    	@media only screen and (max-width: 600px){
-    		nav a{
-    			font-size: 12px;
-    			font-weight: bold;
+	<style type="text/css">
+		body{
+			padding:0;
+			margin: 0;
+		}
+		nav{
+			position: sticky;
+			top: 0px;
+		}
+		
+		footer{
+			
+			width: 100%;
+			text-align: center;
+			bottom: 0;
+		}
+		.section{
+			background: url("ckt.jpg");
+			background-size: cover;
+			height: 80vh;
+		}
+		a:hover{
+			color: yellow;
+		}
+		@media only screen and (max-width: 600px){
+			nav a{
+				font-size: 12px;
+				font-weight: bold;
 
-    		}
-    		nav{
-    			height: auto;
-    			
-    		}
-    	}
-    </style>
+			}
+			nav{
+				height: auto;
+				
+			}
+		}
+	</style>
 </head>
 <body >
 	<nav class="bg-blue-400 text-white  text-3xl">
 		<a class="mx-2 " href="hom.php">Menu</a>
-		<a class="mx-2 " href="liveScore.php">Live Score</a>
+		<a class="mx-2 " href="liveScores.php">Live Score</a>
 		<a class="mx-2 " href="league.html">Schedule</a>
 		<a class="mx-2 " href="news.html">News</a>
 		<a class="mx-2 " href="aboutus.html">Our Team</a>
 	</nav>
 
-	<div class="section">
-		this is my content
+	<div class="section py-5">
+		<div class="match-status bg-white rounded rounded mx-2 shadow-2xl my-2">
+			<h1 class="text-xl">Match Status</h1>
+			<p> <span class="text-red-400">Sorry !</span> <br>Current now match is live.</p>
+		</div>
+
+		<div class="match-status bg-white rounded rounded mx-2 shadow-2xl">
+			<h1 class="text-xl">We will come soon with new match</h1>
+			<p> <span class="text-red-400">Get your eye here !</span> <br>Date: Pendign</p>
+		</div>
 	</div>
 
 	<footer class="bg-black text-white text-2xl">
